8144ed55edeef15f3124f834e9fe9749b7522945
frohrlich <32771547+frohrlich@users.noreply.github.com>
2023-05-05 11:18:02 +0200

    Mise en place chat websocket (tutoriel)

---
+++ src/main/java/com/projet/rpg/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,12 @@
+package com.projet.rpg.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Greeting {
+	private String content;
+}
