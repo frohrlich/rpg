83f78c98f73cb24f0d1d0569b11d683acf9e53fc
frohrlich <32771547+frohrlich@users.noreply.github.com>
2023-05-05 17:24:37 +0200

    test

--- src/main/java/com/projet/rpg/game/GameService.java
+++ src/main/java/com/projet/rpg/game/GameService.java
@@ -17,17 +17,16 @@ import com.projet.rpg.vue.VueAvecPnj;
 
 @Service
 public class GameService {
-	
+
 	private Game game;
-	
+
 	@Autowired
 	private JoueurService joueurService;
 	@Autowired
 	private PnjService pnjService;
 	@Autowired
 	private EvenementDialogueService evenementDialogueService;
-	
-	
+
 	public GameService(Game game, JoueurService joueurService, PnjService pnjService,
 			EvenementDialogueService evenementDialogueService) {
 		super();
@@ -36,30 +35,30 @@ public class GameService {
 		this.pnjService = pnjService;
 		this.evenementDialogueService = evenementDialogueService;
 	}
-	
-	
+
 	/**
 	 * Sert à initialiser le jeu
 	 */
 	public VueAvecPnj initialize() {
-		
+
 		joueurService.deleteAll();
 		pnjService.deleteAll();
-		
-		
-		Personnage perJ = new Personnage(1,"Alex", Sexe.M, Role.Ep, 150, 1280, 2280, 300, 300, 2000, 3, "img/epeisteM.png");
-		Joueur Martin = new Joueur(1,1000,perJ);
-		Personnage perP = new Personnage(2, "Paysanne", Sexe.M, Role.Ep, 150, 1280, 2280, 300, 300, 2000, 3, "img/paysanne.png");
-	    Pnj pnj = new Pnj(1,"",perP);
-	    
+
+		Personnage perJ = new Personnage(1, "Alex", Sexe.M, Role.Ep, 150, 1280, 2280, 300, 300, 2000, 3,
+				"img/epeisteM.png");
+		Joueur Martin = new Joueur(1, 1000, perJ);
+		Personnage perP = new Personnage(2, "Paysanne", Sexe.M, Role.Ep, 150, 1280, 2280, 300, 300, 2000, 3,
+				"img/paysanne.png");
+		Pnj pnj = new Pnj(1, "", perP);
+
 		joueurService.save(Martin);
-		
+
 		// Initialisation du joueur dans le jeu
 		game.setCurrentJoueur(Martin);
-		
+
 		// Création d'un dialogue pour le PNJ
-		String dialoguePnj = PnjService.dialogueCreation(new String[] { "Coucou je serai ton rival", "prépare toi à m'affronter", "c'est parti!" },
-				10, 0);
+		String dialoguePnj = PnjService.dialogueCreation(
+				new String[] { "Coucou je serai ton rival", "prépare toi à m'affronter", "c'est parti!" }, 10, 0);
 		// Assignation du dialogue au PNJ
 		pnj.setDialogue(dialoguePnj);
 		// Update du PNJ dans la BDD, maintenant avec son dialogue
@@ -74,35 +73,36 @@ public class GameService {
 		game.setCurrentVue(evenementDialogueService.nextVue());
 		return game.getCurrentVue();
 	}
-	
+
 	/**
 	 * C'est le coeur même du jeu
+	 * 
 	 * @param message
 	 * @return
 	 */
 	public VueAvecPnj update(String message) {
 		VueAvecPnj nouvelleVue = null;
-		
+
 		JSONObject obj = new JSONObject(message);
 		String strClick = obj.getString("click");
-		
+
 		switch (strClick) {
 		case "option1":
 			nouvelleVue = evenementDialogueService.nextVue();
-			if(nouvelleVue != null) {
+			if (nouvelleVue != null) {
 				game.setCurrentVue(nouvelleVue);
 			}
 			break;
 		case "option2": // On transforme le personnage en ours
 			nouvelleVue = evenementDialogueService.nextVue();
-			if(nouvelleVue != null) {
+			if (nouvelleVue != null) {
 				nouvelleVue.getJoueur().getPersonnage().setApparence("img/le_ours.png");
 				game.setCurrentVue(nouvelleVue);
 			}
 			break;
 		case "option3": // On transforme le background en volcan
 			nouvelleVue = evenementDialogueService.nextVue();
-			if(nouvelleVue != null) {
+			if (nouvelleVue != null) {
 				nouvelleVue.setBackground("img/bg_volcan.png");
 				game.setCurrentVue(nouvelleVue);
 			}
