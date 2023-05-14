a0ce91b3f212ef265c3bfb91780a3360417c6871
frohrlich <32771547+frohrlich@users.noreply.github.com>
2023-05-14 23:18:26 +0200

    nettoyage code inutile

--- src/main/java/com/projet/rpg/personnage/pnj/ennemi/EnnemiRepository.java
+++
@@ -1,5 +0,0 @@
-package com.projet.rpg.personnage.pnj.ennemi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnnemiRepository extends JpaRepository<Ennemi, Integer> {}
